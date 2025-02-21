51e23cfe35de1c439bc10685ebe3730ad0eac466
dinnu5268 <dinesh.puvvala.dp@gmail.com>
2025-02-21 18:07:29 +0530

    dsa

--- function_app.py
+++ function_app.py
@@ -5,7 +5,7 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
 @app.route(route="http_trigger")
 def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.') #hi Suni
+    logging.info('Python HTTP trigger function processed a request.') #hi Suni your leg is great
 
     name = req.params.get('name')
     if not name:
